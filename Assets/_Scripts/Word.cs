df001dec7e7d9981910c5f16aea7bb425eb68aef
Reuben Moorhouse <reuben.moorhouse@gmail.com>
2020-03-08 02:37:30 +1100

    Pushing our word script

---
+++ Assets/_Scripts/Word.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Word : MonoBehaviour
+{
+   public enum WORD_STAT {DEX = 0, STR = 1, CON = 2, WIT = 3};
+   public string word;
+   public int[] stats;
+
+   public int getStat(WORD_STAT stat){
+      return stats[(int)stat];
+   }
+}
