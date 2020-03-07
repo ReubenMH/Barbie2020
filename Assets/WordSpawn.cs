a1bd8d4100a16d00cfc0558b54b8fed1429a4e16
ediebold <elliot@diebold.com.au>
2020-03-08 04:42:35 +1100

    hello Reuben I am your new commit, happy birthday to me.

---
+++ Assets/WordSpawn.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordSpawn : MonoBehaviour
+{
+    private float width = 1;
+    private float height = 1;
+}
--- Assets/_Scripts/Character.cs
+++ Assets/_Scripts/Character.cs
@@ -11,22 +11,35 @@ public class Character : MonoBehaviour
     public Animator anim;
 
     private Vestige ourVestige;
+    private Character other;
 
     private float healthFloat;
     private float newHealth;
 
-    public void Initialize(bool isLeft, Vestige v)
+    public void Initialize(bool isLeft, Vestige v, Character other)
     {
         ourVestige = v;
         anim.SetBool("isLeft", isLeft);
+        this.other = other;
     }
 
     public void SetNewHealth(float f)
     {
         newHealth = f;
+        Debug.Log("new health is " + f);
+    }
+
+    public void Attack()
+    {
+        anim.SetTrigger("attack");
     }
 
     void TakeHit()
+    {
+        other.Bleed();
+    }
+
+    public void Bleed()
     {
         blood.Emit(20);
         healthImage.fillAmount = newHealth;
