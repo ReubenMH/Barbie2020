12c6c3b8a6bf728f81ac051fdff8715cf50d5f5b
Reuben Moorhouse <reuben.moorhouse@gmail.com>
2020-03-08 03:09:16 +1100

    Adding sprite

--- Assets/_Scripts/Word.cs
+++ Assets/_Scripts/Word.cs
@@ -1,14 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
+[Serializable]
 public class Word : MonoBehaviour
 {
    public enum WORD_STAT {DEX = 0, STR = 1, CON = 2, WIT = 3};
+   [SerializeField]
    public string word;
+   [SerializeField]
    public int[] stats;
 
    public int getStat(WORD_STAT stat){
       return stats[(int)stat];
+   }
+
+   public Word(string word, int dex, int str, int con, int wit){
+      this.word = word;
+      this.stats = new int[4];
+      this.stats[0] = dex;
+      this.stats[1] = str;
+      this.stats[2] = con;
+      this.stats[3] = wit;
    }
 }
--- Assets/_Scripts/WordDictionary.cs
+++ Assets/_Scripts/WordDictionary.cs
@@ -1,18 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Json;
+using System.Json.JsonSerializer;
 
 public class WordDictionary : MonoBehaviour
 {
+    public WordDictionary Instance{
+        get{
+            return _inst;
+        }
+    }
+    private WordDictionary _inst;
+
+    public TextAsset wordDictFile;
+
+    public Dictionary<string, Word> wordDict;
+
+    private Word[] wordList;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(_inst == null){
+            _inst = this;
+            DontDestroyOnLoad(gameObject);
+        }else{
+            Destroy(gameObject);
+        }
+
+        TestWordList();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void TestWordList(){
+        wordList = new wordList[5];
+        wordList[0] = new Word("cowardly", 4, 1, 1, 2);
+        wordList[1] = new Word("aggressive", 3, 3, 1, 1);
+        wordList[2] = new Word("fat", 1, 2, 4, 1);
+        wordList[3] = new Word("quick", 5, 1, 1, 3);
+        wordList[4] = new Word("dramatic", 4, 1, 1, 2);
+    }
+
+    void LoadWordDict(){
+        wordList = JsonSerializer.Deserialize<Word[]>(wordDictFile);
+
+        for(int i = 0; i < wordList.Length; i++){
+            wordDict.Add(wordList[i].word, wordList[i]);
+        }
+    }
+
+    void SaveWordDict(){
+        string json = JsonSerializer.Serialize(wordList);
+
+        Debug.Log(json);
     }
 }
